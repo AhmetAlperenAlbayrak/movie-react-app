12fbf84097d24e93cb89857dc82ab515f3c4f597
[Ahmet Albayrak] <[ahmetalperenalbayrak@gmail.com]>
2022-04-19 02:57:23 +0300

    Movie titles fixed

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
     return (
-        <div className='movie'>
-        <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://via.placeholder.com/400'} alt={movie.Title}/>
-        <div className='movie-info'>
-            <h2>{movie.Title}</h2>
-            <p>{movie.Year}</p>
-        
+      <div className="movie" key={imdbID}>
+        <div>
+          <p>{Year}</p>
         </div>
-        
-    </div>
-    )
-}
+  
+        <div>
+          <img src={Poster !== "N/A" ? Poster : "https://via.placeholder.com/400"} alt={Title} />
+        </div>
+  
+        <div>
+          <span>{Type}</span>
+          <h3>{Title}</h3>
+        </div>
+      </div>
+    );
+  }
 
 export default MovieCard;
